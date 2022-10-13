f38c4123f3ad76dd378c7522339ae90df17f9cb3
Amirabbas Fazelinia <utilyre@proton.me>
2022-10-13 18:02:58 +0330

    feat(pwa): configure vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/methematics/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Methematics',
+        short_name: 'Methematics',
+        description: 'Find all the possible divisors of a given number.',
+        background_color: '#293241',
+        theme_color: '#3d5a80',
+        icons: [
+          {
+            src: '/methematics-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/methematics-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
